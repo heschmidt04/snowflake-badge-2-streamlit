de2a8038fd30290c23a1fe8a73b3ecc0e0d46016
Heidi E. Schmidt <bb00p04@gmail.com>
2023-07-13 08:48:24 -0400

    Update streamlit_app.py
    Add pd df for the JSON response text

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Using Pandas read the JSON into a dataframe table using streamlit
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Format the data into a table with columns 
+streamlit.dataframe(fruityvice_normalized)
