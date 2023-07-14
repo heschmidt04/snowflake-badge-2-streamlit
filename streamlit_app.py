6cc2d1ba7f6c59d8ebbb1aa2c331b59b7dab71a9
Heidi E. Schmidt <bb00p04@gmail.com>
2023-07-14 10:45:05 -0400

    Update streamlit_app.py
    adding streamlit write and cursor insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,7 @@ streamlit.dataframe(my_data_rows)
 # add_my_fruit = 
 streamlit.text_input('What fruit would you like information about?',key = "add_my_fruit")
 
-# my_cur.execute("INSERT into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values (add_my_fruit)") 
+streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("INSERT into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values (from streamlit)") 
 
 
