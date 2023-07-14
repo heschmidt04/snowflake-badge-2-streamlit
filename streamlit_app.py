6de842fd662dbe721c4f4ab661827f3aef85a3ff
Heidi E. Schmidt <bb00p04@gmail.com>
2023-07-14 10:50:11 -0400

    Update streamlit_app.py
    soft quotes needed

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,6 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like information about?', key = "add_my_fruit")
 
 streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("INSERT into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values (from streamlit)") 
+# my_cur.execute("INSERT into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')") 
 
 
